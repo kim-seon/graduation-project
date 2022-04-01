4c24ce0c4d7ffafaf8b1ee741665971acafa5baa
kim-seon <91369649+kim-seon@users.noreply.github.com>
2022-04-01 14:58:58 +0900

    5주차

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
   <React.StrictMode>
--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
+
+const config = require('./Config/key')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI)
+        .then(() => console.log('MongoDB Connected...'))
+        .catch(err => console.log(err))
+
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
